93103fdaa2bfe26e5eddbd97607d3d1a0606a439
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2021-10-06 21:38:11 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -17,21 +17,21 @@ func Init() *echo.Echo {
 
 	//Admin
 	e.GET("/admin", controllers.TakeAdmin, middleware.IsAutenthicated)
-	e.POST("/admin", controllers.AllAdmin)
-	e.PUT("/admin", controllers.UpdateAdmin)
-	e.DELETE("/admin", controllers.DeleteAdmin)
+	e.POST("/admin", controllers.AllAdmin, middleware.IsAutenthicated)
+	e.PUT("/admin", controllers.UpdateAdmin, middleware.IsAutenthicated)
+	e.DELETE("/admin", controllers.DeleteAdmin, middleware.IsAutenthicated)
 
 	//Product
 	e.GET("/product", controllers.TakeProduct)
-	e.POST("/product", controllers.AllProduct)
-	e.PUT("/product", controllers.UpdateProduct)
-	e.DELETE("/product", controllers.DeleteProduct)
+	e.POST("/product", controllers.AllProduct, middleware.IsAutenthicated)
+	e.PUT("/product", controllers.UpdateProduct, middleware.IsAutenthicated)
+	e.DELETE("/product", controllers.DeleteProduct, middleware.IsAutenthicated)
 
 	//Event
 	e.GET("/event", controllers.TakeEvent)
-	e.POST("/event", controllers.AllEvent)
-	e.PUT("/event", controllers.UpdateEvent)
-	e.DELETE("/event", controllers.DeleteEvent)
+	e.POST("/event", controllers.AllEvent, middleware.IsAutenthicated)
+	e.PUT("/event", controllers.UpdateEvent, middleware.IsAutenthicated)
+	e.DELETE("/event", controllers.DeleteEvent, middleware.IsAutenthicated)
 
 	//Login
 	e.GET("/generate-hash/:password", controllers.GenerateHashPassword)
